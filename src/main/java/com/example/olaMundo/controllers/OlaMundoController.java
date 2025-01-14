b7e79ee490041d6b216e5cafddf2041972ae7550
Lara Oliveira <lara.oliveira@zup.com.br>
2025-01-14 16:30:01 -0300

    aplica "Olá, mundo!" no endereço http://localhost:8080

---
+++ src/main/java/com/example/olaMundo/controllers/OlaMundoController.java
@@ -0,0 +1,12 @@
+package com.example.olaMundo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OlaMundoController {
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Olá, mundo!";
+    }
+}
